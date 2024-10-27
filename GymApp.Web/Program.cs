100f2b8393df436260209ddfcf852020512a7ed0
mcottle1 <kenzcottle@gmail.com>
2024-10-27 13:38:52 -0600

    start on dashboard

--- GymApp.Web/Program.cs
+++ GymApp.Web/Program.cs
@@ -25,8 +25,8 @@ if (!app.Environment.IsDevelopment())
 
 // app.UseHttpsRedirection();
 
-app.UseStaticFiles();
 app.UseAntiforgery();
+app.UseStaticFiles();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
