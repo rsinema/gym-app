dfc2eb95a78437b18b04a1736db4ad8361e67331
Sarah Gonzalez <sarahg.3545@gmail.com>
2024-11-15 14:34:54 -0700

    exercise stuff

---
+++ GymApp.Services/ExerciseService/ExerciseService.cs
@@ -0,0 +1,31 @@
+﻿using GymApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApp.Services.ExerciseService;
+public class ExerciseService(IExerciseRepository exerciseRepository) : IExerciseService
+{
+    public async Task<Exercise> GetExerciseAsync(int id)
+    {
+        return await exerciseRepository.GetExerciseAsync(id);
+    }
+
+    public async Task<List<Exercise>> GetAllExercisesAsync(int userId)
+    {
+        return await exerciseRepository.GetAllExercisesAsync(userId);
+    }
+
+    public async Task<Exercise> AddExerciseAsync(Exercise exercise)
+    {
+        return await exerciseRepository.AddExerciseAsync(exercise);
+    }
+
+
+
+
+
+}
+
---
+++ GymApp.Services/ExerciseService/IExerciseService.cs
@@ -0,0 +1,20 @@
+﻿using GymApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApp.Services.ExerciseService
+{
+    internal interface IExerciseService
+    {
+        Task<Exercise> GetExerciseAsync(int id);
+
+        Task<List<Exercise>> GetAllExercisesAsync(int userId);
+
+        Task<Exercise> AddExerciseAsync(Exercise exercise);
+
+
+    }
+}
